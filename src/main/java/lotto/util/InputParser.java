e0872ded0cd16e48672fad9a7da33cd737391b53
poketopa <lhs5427ll@gmail.com>
2025-10-31 22:49:14 +0900

    feat(util): 당첨 번호 파싱 메서드 추가

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -1,4 +1,24 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputParser {
+    private InputParser(){}
+
+    public static int parseBuyAmount(String buyAmount){
+        return Integer.parseInt(buyAmount);
+    }
+
+    public static List<Integer> parseWinningNumbers(String winningNumbers){
+        return Arrays.stream(winningNumbers.split(","))
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    public static int parseBonusNumber(String bonusNumber){
+        return Integer.parseInt(bonusNumber);
+    }
 }
