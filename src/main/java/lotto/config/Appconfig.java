99579b2993a8430f00148039c528b2c3c448acbf
poketopa <lhs5427ll@gmail.com>
2025-10-31 17:03:23 +0900

    feat(config): 의존성 주입 Appconfig 객체 생성

--- src/main/java/lotto/config/Appconfig.java
+++ src/main/java/lotto/config/Appconfig.java
@@ -1,4 +1,24 @@
 package lotto.config;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Appconfig {
+    public LottoService lottoService(){
+        return new LottoService();
+    }
+
+    public InputView inputView(){
+        return new InputView();
+    }
+
+    public OutputView outputView(){
+        return new OutputView();
+    }
+
+    public LottoController lottoController(){
+        return new LottoController(lottoService(), inputView(), outputView());
+    }
 }
