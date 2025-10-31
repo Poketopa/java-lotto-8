2520628ed563afdec174b8d746db22f5a24c950f
poketopa <lhs5427ll@gmail.com>
2025-10-31 20:14:48 +0900

    feat(domain): RandomNumberGenerator 랜덤값 생성기 도메인 추가

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -1,4 +1,15 @@
 package lotto.domain;
 
-public class RandomNumberGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator implements LottoGenerator{
+    private final int MIN_LOTTO_NUMBER = 1;
+    private final int MAX_LOTTO_NUMBER = 45;
+    private final int PICK_NUMBERS = 6;
+
+    @Override
+    public List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, PICK_NUMBERS);
+    }
 }
