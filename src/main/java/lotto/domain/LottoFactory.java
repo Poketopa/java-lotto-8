65c959b1d9f5b2c5bed465a26c9c020a79b11831
poketopa <lhs5427ll@gmail.com>
2025-10-31 20:13:45 +0900

    feat(domain): 로또 생성 로직에 팩토리 패턴 추가

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoFactory {
+    private final LottoGenerator lottoGenerator;
+
+    public LottoFactory(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public Lotto generateLotto(){
+        List<Integer> numbers = lottoGenerator.generateLotto();
+        return new Lotto(numbers);
+    }
 }
