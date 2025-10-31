e6aff6c3a1ae391716885f5e32c5d9971ab30e12
poketopa <lhs5427ll@gmail.com>
2025-10-31 12:01:29 +0900

    feat(domain): 구입 금액 일급 컨렉션 BuyAmount 도메인 및 검증 메서드 생성

--- src/main/java/lotto/domain/BuyAmount.java
+++ src/main/java/lotto/domain/BuyAmount.java
@@ -1,4 +1,14 @@
 package lotto.domain;
 
-public class BuyingAmount {
+public class BuyAmount {
+    private final int buyAmount;
+
+    public BuyAmount(int buyAmount) {
+        validate(buyAmount);
+        this.buyAmount = buyAmount;
+    }
+
+    private void validate(int buyAmount){
+
+    }
 }
