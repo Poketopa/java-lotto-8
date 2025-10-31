1b98a3e10f777cd83070ee189ef4c82e3eb95863
poketopa <lhs5427ll@gmail.com>
2025-10-31 22:47:37 +0900

    feat(domain): 로또 정보 getter 메서드 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,12 +1,27 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lottos {
     private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
+        validate(lottos);
         this.lottos = lottos;
     }
 
+    private void validate(List<Lotto> lottos){
+
+    }
+
+    public List<String> getLottos(){
+        return lottos.stream()
+                .map(Lotto::toString)
+                .collect(Collectors.toList());
+    }
+
+    public int getBuyAmout(){
+        return lottos.size();
+    }
 }
