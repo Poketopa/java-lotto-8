d5363ba877601ea45e0291454f78c7c3c9ccea98
poketopa <lhs5427ll@gmail.com>
2025-10-31 22:22:16 +0900

    refactor(view): 입/출력 메시지 Messages 객체 연결

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,22 +3,18 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static final String BUY_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    public static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
-    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
-
     public String inputBuyAmount(){
-        System.out.println(BUY_AMOUNT_INPUT_MESSAGE);
+        System.out.println(Messages.BUY_AMOUNT_INPUT_MESSAGE);
         return Console.readLine();
     }
 
     public String inputWinningNumber(){
-        System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+        System.out.println(Messages.WINNING_NUMBER_INPUT_MESSAGE);
         return Console.readLine();
     }
 
     public String inputBonusNumber(){
-        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        System.out.println(Messages.BONUS_NUMBER_INPUT_MESSAGE);
         return Console.readLine();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
+import lotto.dto.LottoResult;
+
 public class OutputView {
+    public void printGeneratedLotto(LottoResult lottoResult) {
+        System.out.println(String.format(Messages.BUY_COUNT_MESSAGE, lottoResult.buyAmount()));
+        lottoResult.lottos().forEach(System.out::println);
+    }
 }
