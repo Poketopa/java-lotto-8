01b20fd4e6e8d3c46e7402e40bcc451cd83b5433
poketopa <lhs5427ll@gmail.com>
2025-10-31 22:47:02 +0900

    feat(controller): 보너스 번호 입력 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,10 @@
 package lotto.controller;
 
+import lotto.domain.BonusNumber;
 import lotto.domain.BuyAmount;
 import lotto.domain.Lottos;
 import lotto.domain.WinningNumbers;
+import lotto.dto.LottoResult;
 import lotto.service.LottoService;
 import lotto.util.InputParser;
 import lotto.view.InputView;
@@ -23,16 +25,16 @@ public class LottoController {
         // 구입 금액 입력
         String rawBuyAmount = inputView.inputBuyAmount();
         Lottos lottos = lottoService.buyLotto(rawBuyAmount);
+        // 구입 개수 출력
+        outputView.printGeneratedLotto(new LottoResult(lottos.getBuyAmout(), lottos.getLottos()));
+
         // 당첨 번호 입력
         String rawWinningNumbers = inputView.inputWinningNumber();
         WinningNumbers winningNumbers = lottoService.createWinningNumbers(rawWinningNumbers);
         // 보너스 번호 입력
+        String rawBonusNumber = inputView.inputBonusNumber();
+        BonusNumber bonusNumber = lottoService.createBonusNumber(rawBonusNumber);
 
 
-        // 검증
     }
-
-
-
-
 }
