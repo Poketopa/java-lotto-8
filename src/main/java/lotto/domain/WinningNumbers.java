474a4d24012bbf8722658333affc87715bc8948b
poketopa <lhs5427ll@gmail.com>
2025-10-31 12:04:12 +0900

    feat(domain): 담청 번호 일급 컨렉션 WinningNumbers 도메인 및 검증 메서드 생성

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+
+    public WinningNumbers(List<Integer> winningNumbers) {
+        validate(winningNumbers);
+        this.winningNumbers = winningNumbers;
+    }
+
+    private void validate(List<Integer> winningNumbers){
+
+    }
 }
