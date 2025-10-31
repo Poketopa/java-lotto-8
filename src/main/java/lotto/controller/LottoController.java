73ae3883b1ae1591659afbd342a25a6d577a5c78
poketopa <lhs5427ll@gmail.com>
2025-10-31 17:01:10 +0900

    feat(controller): 컨트롤러 생성 및 의존성 주입

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,35 @@
 package lotto.controller;
 
+import lotto.domain.BuyAmount;
+import lotto.service.LottoService;
+import lotto.util.InputParser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoController {
+    private final LottoService lottoService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(LottoService lottoService, InputView inputView, OutputView outputView) {
+        this.lottoService = lottoService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+    // 구입 금액 입력
+
+    // 검증
+
+    // 구입 갯수 DTO로 담아서 서비스로 넘김 -> 로또 도메인에서 랜던값 생성
+
+    // 당첨 번호 입력
+
+    // 검증
+
+    // 보너스 번호 입력
+
+    // 검증
+
+
+
 }
