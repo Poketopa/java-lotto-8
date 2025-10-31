f5b692eb60fd87ee730632f0e49684ea065a5f99
poketopa <lhs5427ll@gmail.com>
2025-10-31 20:11:57 +0900

    feat(config): 로또 팩토리 생성 후 의존성 주입

--- src/main/java/lotto/config/Appconfig.java
+++ src/main/java/lotto/config/Appconfig.java
@@ -1,13 +1,24 @@
 package lotto.config;
 
 import lotto.controller.LottoController;
+import lotto.domain.LottoFactory;
+import lotto.domain.LottoGenerator;
+import lotto.domain.RandomNumberGenerator;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Appconfig {
+    public LottoGenerator lottoGenerator(){
+        return new RandomNumberGenerator();
+    }
+
+    public LottoFactory lottoFactory(){
+        return new LottoFactory(lottoGenerator());
+    }
+
     public LottoService lottoService(){
-        return new LottoService();
+        return new LottoService(lottoFactory());
     }
 
     public InputView inputView(){
