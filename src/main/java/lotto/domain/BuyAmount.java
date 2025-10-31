671453566ea4922f425faadcec3e28a9c73a5ecc
poketopa <lhs5427ll@gmail.com>
2025-10-31 20:12:59 +0900

    feat(domain): BuyAmount 도메인 검증 메서드 추가

--- src/main/java/lotto/domain/BuyAmount.java
+++ src/main/java/lotto/domain/BuyAmount.java
@@ -9,6 +9,23 @@ public class BuyAmount {
     }
 
     private void validate(int buyAmount){
+        checkNegativeNumber(buyAmount);
+        checkMultipleOfThousand(buyAmount);
+    }
 
+    private void checkNegativeNumber(int buyAmount){
+        if(buyAmount <= 0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkMultipleOfThousand(int buyAmount){
+        if(buyAmount % 1000 != 0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getBuyAmount(){
+        return buyAmount;
     }
 }
