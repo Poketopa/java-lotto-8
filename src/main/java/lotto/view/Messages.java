7b190f37bf9a9ee665499a91d0777df0ddf57887
poketopa <lhs5427ll@gmail.com>
2025-10-31 22:21:41 +0900

    feat(view): 메시지 관리 객체 생성

---
+++ src/main/java/lotto/view/Messages.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class Messages {
+    public static final String BUY_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static final String BUY_COUNT_MESSAGE = "%d개를 구매했습니다.";
+
+    private Messages(){}
+}
