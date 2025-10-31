e2a93d4765c56bf5e697749362216598ab9ce4f8
poketopa <lhs5427ll@gmail.com>
2025-10-31 20:15:29 +0900

    feat(service): 서비스의 팩토리 의존 추가 및 buyLotto 메서드 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,28 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.BuyAmount;
+import lotto.domain.Lotto;
+import lotto.domain.LottoFactory;
+import lotto.domain.Lottos;
+import lotto.util.InputParser;
+
 public class LottoService {
+    private final LottoFactory lottoFactory;
+
+    public LottoService(LottoFactory lottoFactory) {
+        this.lottoFactory = lottoFactory;
+    }
+
+    public Lottos buyLotto(String rawBuyAmount){
+        BuyAmount buyAmount = new BuyAmount(InputParser.parseBuyAmount(rawBuyAmount));
+
+        int lottoCount = buyAmount.getBuyAmount();
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i=0;i<lottoCount;i++){
+            lottos.add(lottoFactory.generateLotto());
+        }
+        return new Lottos(lottos);
+    }
 }
