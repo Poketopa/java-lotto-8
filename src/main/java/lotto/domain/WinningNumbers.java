dddf92ceedf833f45e4fd744d88692602b64ec3f
poketopa <lhs5427ll@gmail.com>
2025-11-02 22:37:53 +0900

    feat(domain): 당첨 번호 검증 메서드 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.exception.ErrorCode;
 
 public class WinningNumbers {
     private final List<Integer> winningNumbers;
@@ -11,7 +15,30 @@ public class WinningNumbers {
     }
 
     private void validate(List<Integer> winningNumbers){
+        checkWinningNumberRange(winningNumbers);
+        checkWinningNumberSize(winningNumbers);
+        checkWinningNumberDuplication(winningNumbers);
+    }
 
+    private void checkWinningNumberRange(List<Integer> winningNumbers){
+        for(int winningNumber : winningNumbers){
+            if(winningNumber < 1 || winningNumber > 45){
+                throw new IllegalArgumentException(ErrorCode.INVALID_WINNING_NUMBER_RANGE.message());
+            }
+        }
+    }
+
+    private void checkWinningNumberSize(List<Integer> winningNumbers) {
+        if(winningNumbers.size() != 6){
+            throw new IllegalAccessError(ErrorCode.INVALID_WINNING_NUMBER_SIZE.message());
+        }
+    }
+
+    private void checkWinningNumberDuplication(List<Integer> winningNumbers){
+        Set<Integer> set = new HashSet<>(winningNumbers);
+        if(set.size() != 6){
+            throw new IllegalArgumentException(ErrorCode.WINNING_NUMBER_DUPLICATION.message());
+        }
     }
 
     public boolean contains(int inputNumber){
