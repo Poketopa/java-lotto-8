a17b50805a29e51973c8e6452b8b2cbf2c0730f7
poketopa <lhs5427ll@gmail.com>
2025-11-02 13:05:37 +0900

    feat(domain): 로또 상금 정보를 담는 enum 클래스 생성

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,4 +1,20 @@
 package lotto.domain;
 
 public enum Prize {
+    PRIZE_FIRST(2000000000L),
+    PRIZE_SECOND(30000000L),
+    PRIZE_THIRD(1500000L),
+    PRIZE_FOURTH(50000L),
+    PRIZE_FIFTH(5000L),
+    NO_PRIZE(0L);
+
+    private final long prizeMoney;
+
+    Prize(long prizeMoney){
+        this.prizeMoney = prizeMoney;
+    }
+
+    public long getPrizeMoney(){
+        return prizeMoney;
+    }
 }
