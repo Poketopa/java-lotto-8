52d38f63181fc235eb1723b7750e880d2e766025
poketopa <lhs5427ll@gmail.com>
2025-11-02 13:11:16 +0900

    feat(view): 로또 당첨 결과 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,11 @@
 package lotto.view;
 
+import java.math.BigDecimal;
+import java.util.Map;
 import java.util.stream.Collectors;
+import lotto.domain.Prize;
 import lotto.dto.LottoResult;
+import lotto.dto.ResultResopnse;
 
 public class OutputView {
     public void printGeneratedLotto(LottoResult lottoResult) {
@@ -10,5 +14,17 @@ public class OutputView {
                 .sorted()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", ")) + "]");
+    }
+
+    public void printLottoResult(ResultResopnse resultResopnse){
+        Map<Prize, Integer> lottoResult = resultResopnse.winningResult().getResult();
+        BigDecimal profitRate = resultResopnse.profitRate();
+
+        System.out.println(Messages.WINNING_STAT_MESSAGE);
+        System.out.println(String.format(Messages.PRIZE_FIFTH, lottoResult.get(Prize.PRIZE_FIFTH)));
+        System.out.println(String.format(Messages.PRIZE_FOURTH, lottoResult.get(Prize.PRIZE_FOURTH)));
+        System.out.println(String.format(Messages.PRIZE_THIRD, lottoResult.get(Prize.PRIZE_THIRD)));
+        System.out.println(String.format(Messages.PRIZE_SECOND, lottoResult.get(Prize.PRIZE_SECOND)));
+        System.out.println(String.format(Messages.PRIZE_FIRST, lottoResult.get(Prize.PRIZE_FIRST)));
     }
 }
