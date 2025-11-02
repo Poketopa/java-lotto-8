a84b2b8ea10f2529c570f40412c6d9d08f2dea48
poketopa <lhs5427ll@gmail.com>
2025-11-02 23:36:49 +0900

    feat(domain): 로또 규칙 상수 클래스 추가

---
+++ src/main/java/lotto/domain/LottoInfo.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public final class LottoInfo {
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_MIN = 1;
+    public static final int LOTTO_MAX = 45;
+    public static final int LOTTO_PRICE = 1000;
+
+    private LottoInfo() {}
+}
+
+
--- src/main/java/lotto/exception/error/InvalidInputFormatException.java
+++
@@ -1,7 +0,0 @@
-package lotto.exception.error;
-
-public class InvalidInputFormatException extends RuntimeException {
-  public InvalidInputFormatException(String message) {
-    super(message);
-  }
-}
