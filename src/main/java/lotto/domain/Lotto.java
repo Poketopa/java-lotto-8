4855546849278f831d31d074c93fcad004456e0d
poketopa <lhs5427ll@gmail.com>
2025-11-02 13:04:24 +0900

    feat(domin): 로또 도메인 포함 숫자 반환 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
-package lotto;
+package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +17,14 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public boolean contains(int inputNumber){
+        if(numbers.contains(inputNumber)){
+            return true;
+        }
+        return false;
+    }
+
+    public List<Integer> getNumbers(){
+        return new ArrayList<>(numbers);
+    }
 }
