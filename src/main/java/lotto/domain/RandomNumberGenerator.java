cff8e7e467fc578de1512c134322e2fb65e2d98f
poketopa <lhs5427ll@gmail.com>
2025-11-02 23:38:39 +0900

    refactor(domain): 난수 생성 범위/개수 LottoInfo 상수 적용

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -4,12 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomNumberGenerator implements LottoGenerator{
-    private final int MIN_LOTTO_NUMBER = 1;
-    private final int MAX_LOTTO_NUMBER = 45;
-    private final int PICK_NUMBERS = 6;
 
     @Override
     public List<Integer> generateLotto() {
-        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, PICK_NUMBERS);
+        return Randoms.pickUniqueNumbersInRange(LottoInfo.LOTTO_MIN, LottoInfo.LOTTO_MAX, LottoInfo.LOTTO_SIZE);
     }
 }
