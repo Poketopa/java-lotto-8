f7ba6422842a7d204d145fb9725cbfedb011cbcc
poketopa <lhs5427ll@gmail.com>
2025-11-02 13:13:14 +0900

    feat(view): 결과 출력 메시지 상수 추가

--- src/main/java/lotto/view/Messages.java
+++ src/main/java/lotto/view/Messages.java
@@ -7,5 +7,12 @@ public class Messages {
 
     public static final String BUY_COUNT_MESSAGE = "%d개를 구매했습니다.";
 
+    public static final String WINNING_STAT_MESSAGE = "당첨 통계\n---";
+    public static final String PRIZE_FIRST = "6개 일치 (2,000,000,000원) - %d개";
+    public static final String PRIZE_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String PRIZE_THIRD = "5개 일치 (1,500,000원) - %d개";
+    public static final String PRIZE_FOURTH = "4개 일치 (50,000원) - %d개";
+    public static final String PRIZE_FIFTH = "3개 일치 (5,000원) - %d개";
+
     private Messages(){}
 }
