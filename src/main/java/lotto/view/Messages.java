d8f5b3fc0325dc9d65987be5a02588b4ab846dc6
poketopa <lhs5427ll@gmail.com>
2025-11-02 19:37:19 +0900

    feat(view): 총 수익률 메시지 상수 추가

--- src/main/java/lotto/view/Messages.java
+++ src/main/java/lotto/view/Messages.java
@@ -13,6 +13,7 @@ public class Messages {
     public static final String PRIZE_THIRD = "5개 일치 (1,500,000원) - %d개";
     public static final String PRIZE_FOURTH = "4개 일치 (50,000원) - %d개";
     public static final String PRIZE_FIFTH = "3개 일치 (5,000원) - %d개";
+    public static final String YIELD_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     private Messages(){}
 }
