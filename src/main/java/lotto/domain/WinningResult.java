983ad44e2486a20bdc4b9ade0e9d27e1e33a2763
poketopa <lhs5427ll@gmail.com>
2025-11-02 13:11:33 +0900

    feat(domain): 등수 당 당첨 갯수를 담는 Map 일급 컬렉션 WinningResult 추가

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -1,4 +1,31 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WinningResult {
+    private final Map<Prize, Integer> winningResult;
+
+    public WinningResult(Map<Prize, Integer> winningResult) {
+        this.winningResult = winningResult;
+    }
+
+    public void put(Prize prize){
+        winningResult.put(prize, winningResult.getOrDefault(prize, 0) + 1);
+    }
+
+    public int get(Prize prize){
+        return winningResult.get(prize);
+    }
+
+    public boolean checkPrize(Prize prize){
+        if(winningResult.containsKey(prize)){
+            return true;
+        }
+        return false;
+    }
+
+    public Map<Prize, Integer> getResult(){
+        return new HashMap<>(winningResult);
+    }
 }
