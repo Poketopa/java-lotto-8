b6f0aa43fb221aaba8cc772b84cbf079c37515e8
poketopa <lhs5427ll@gmail.com>
2025-11-02 13:12:53 +0900

    feat(util): 몇 등 당첨인지 확인하는 유틸 클래스 추가

--- src/main/java/lotto/util/WinningChecker.java
+++ src/main/java/lotto/util/WinningChecker.java
@@ -1,4 +1,26 @@
 package lotto.util;
 
+import lotto.domain.Prize;
+
 public class WinningChecker {
+    private WinningChecker(){}
+
+    public static Prize checkWinning(int correctNumberCount, boolean isBonusNumberCorrect){
+        if(correctNumberCount == 6){
+            return Prize.PRIZE_FIRST;
+        }
+        if(correctNumberCount == 5 && isBonusNumberCorrect){
+            return Prize.PRIZE_SECOND;
+        }
+        if(correctNumberCount == 5){
+            return Prize.PRIZE_THIRD;
+        }
+        if(correctNumberCount == 4){
+            return Prize.PRIZE_FOURTH;
+        }
+        if(correctNumberCount == 3){
+            return Prize.PRIZE_FIFTH;
+        }
+        return Prize.NO_PRIZE;
+    }
 }
